0cab0f7bc4f72b52e531ba038e2e683498d053fd
Devin <devinpadron28@gmail.com>
2023-04-30 09:06:09 -0400

    Ensure 0 isn't concatenated on by another 0. If equals sign is pressed before selecting an operator, do nothing.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let replaceTracker = true; // If equal to true will replace displayed value
     let numTracker = true; // If true then value will be assigned to num1, else num2
     let equalsClicked = false; // if clear button was just clicked
+    let operatorClicked = true;
+    let operatorCounter = 0;
     
     // Operations
     let sum = (num1, num2) => num1 + num2;
@@ -38,55 +40,66 @@ document.addEventListener("DOMContentLoaded", () => {
     numberedButtons.forEach((button) => {
         // Add an event listener when a button is clicked
         button.addEventListener("click", () => {
-            // TODO: Prevent number from being concatenated onto zero (i.e. 0024).
-            if (equalsClicked) {
-                resetVariables();
+            // Prevent number from being concatenated onto zero (i.e. 0024).
+            if (button.textContent == 0 && displayMain.textContent == 0) {
+                // do nothing 
             } else {
-                equalsClicked = false;
-            }
-            // Replace value being displayed
-            if (replaceTracker) {
-                // Change the display to whatever was clicked.
-                displayMain.textContent = button.textContent;
-                if (numTracker) {
-                    displayEqn.textContent = button.textContent;
-                    num1 = displayMain.textContent;
+                // reset values if equals was previously clicked without operator in between
+                if (equalsClicked) {
+                    resetVariables();
                 } else {
-                    displayEqn.textContent += button.textContent;
-                    num2 = displayMain.textContent;
+                    equalsClicked = false;
+                    operatorClicked = false;
                 }
-                // No longer replace number
-                replaceTracker = false;
-            } else {
-                displayMain.textContent += button.textContent;
-                displayEqn.textContent += button.textContent;
-                // Concatenate text
-                numTracker ? (num1 += button.textContent) : (num2 += button.textContent);
-            } 
+                // Replace value being displayed
+                if (replaceTracker) {
+                    // Change the display to whatever was clicked.
+                    displayMain.textContent = button.textContent;
+                    if (numTracker) {
+                        displayEqn.textContent = button.textContent;
+                        num1 = displayMain.textContent;
+                    } else {
+                        displayEqn.textContent += button.textContent;
+                        num2 = displayMain.textContent;
+                    }
+                    // No longer replace number
+                    replaceTracker = false;
+                } else {
+                    displayMain.textContent += button.textContent;
+                    displayEqn.textContent += button.textContent;
+                    // Concatenate text
+                    numTracker ? (num1 += button.textContent) : (num2 += button.textContent);
+                } 
+            }
         })  
     })
 
     // Operator Buttons
     operatorButtons.forEach((button) => {
         button.addEventListener("click", () => {
+            // TODO: Fix issue where operator can be pressed consecutively.
+            // if operator was clicked back-to-back times
+            if (operatorClicked) { 
+                displayEqn.textContent = num1 + ' ' + button.textContent + ' ';
+            } else {
+                operatorCounter ++;
+                if (operatorCounter == 2) {
+                    equalsOperation();
+                    operatorCounter --;
+                }
+            }
             displayEqn.textContent += ` ${button.textContent} `;
             operator = button.textContent;
             replaceTracker = true;
             numTracker = false;
+            equalsClicked = false;
+            operatorClicked = true;
         })  
     })
 
     // Equals Operator
     equalsButton.addEventListener("click", () => {
-        total = operate(operator, Number(num1), Number(num2));
-        displayMain.textContent = total;
-        displayEqn.textContent = total;
-        num1 = total;
-        num2 = 0;
-        replaceTracker = true;
-        numTracker = false;
-        // TODO: Clear both displays after a number is pressed after equal sign (result)
-        equalsClicked = true;
+        equalsOperation();
     })
 
     // Clear button
@@ -103,8 +116,28 @@ document.addEventListener("DOMContentLoaded", () => {
         total = 0;
         replaceTracker = true;
         numTracker = true;
+        operatorClicked = false;
+        operatorCounter = 0;
+        operatorClicked = false;
+    }
+
+    function equalsOperation() {
+        if (operatorCounter > 0) {
+            total = operate(operator, Number(num1), Number(num2));
+            displayMain.textContent = total;
+            displayEqn.textContent = total;
+            num1 = total;
+            num2 = 0;
+            replaceTracker = true;
+            numTracker = false;
+            equalsClicked = true;
+            operatorClicked = false;
+        }
     }
 });
 
 
+// TODO: After equals sign is pressed, only one digit number is displayed 
 // TODO: Make display text shrink if number of digits is large enough
+// TODO: Add decimal functionality
+// TODO: Listen for keyboard input
