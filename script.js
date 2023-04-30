141129e3327e16098a8d0c4bcaff2594ad508af4
Devin <devinpadron28@gmail.com>
2023-04-30 09:48:32 -0400

    Add negative number functionality.

--- script.js
+++ script.js
@@ -77,7 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Operator Buttons
     operatorButtons.forEach((button) => {
         button.addEventListener("click", () => {
-            // TODO: Fix issue where operator can be pressed consecutively.
             // if operator was clicked back-to-back times
             if (operatorClicked) { 
                 displayEqn.textContent = num1 + ' ' + button.textContent + ' ';
@@ -104,12 +103,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Clear button
     clearButton.addEventListener("click", () => {
-        displayMain.textContent = 0;
-        displayEqn.textContent = 0;
+        updateDisplay(0);
         resetVariables();
         equalsClicked = false;
     })
     
+    // Sign change
+    signChange.addEventListener("click", () => {
+        // if in num1 and number isnt 0
+        if (operatorCounter == 0 && num1 !== 0) {
+            num1 = num1 * -1;
+            updateDisplay(num1);
+        } else if (operatorCounter > 0 && num2 !== 0) { // else if in num2 and number isnt 0
+            num2 = num2 * -1;
+            displayMain.textContent = num2;
+            displayEqn.textContent = num1 + ' ' + operator + num2 + ' ';
+        }
+    })
+
     function resetVariables() {
         num1 = 0;
         num2 = 0;
@@ -124,8 +135,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function equalsOperation() {
         if (operatorCounter > 0 && !equalsClicked) {
             total = operate(operator, Number(num1), Number(num2));
-            displayMain.textContent = total;
-            displayEqn.textContent = total;
+            updateDisplay(total);
             num1 = total;
             num2 = 0;
             replaceTracker = true;
@@ -134,10 +144,14 @@ document.addEventListener("DOMContentLoaded", () => {
             operatorClicked = false;
         }
     }
+
+    function updateDisplay(value) {
+        displayMain.textContent = value;
+        displayEqn.textContent = value;
+    }
 });
 
-
-// TODO: After equals sign is pressed, only one digit number is displayed 
 // TODO: Make display text shrink if number of digits is large enough
 // TODO: Add decimal functionality
+// TODO: Add negative number functionality
 // TODO: Listen for keyboard input
